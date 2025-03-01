db683f7deb9e9ec841f058a1757b18c268039863
hermocillapiona <>
2025-03-01 18:29:45 +0800

    Last Sync: 2025-03-01 10:29:45 (Mobile)

--- if_else_statement.py
+++ if_else_statement.py
@@ -4,7 +4,4 @@ if number % 2 == 0:
     print("The number", number, "is Even.")
 else:
     print("The number", number, "is Odd.")
-try:
-    #code here
-except ValueError:
-    print("Invalid input. Please enter an integer.")
+
